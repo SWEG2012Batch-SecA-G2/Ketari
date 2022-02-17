cc37808cc4ff14e7f78aa51bce3e7f0bcd379557
BrookFeleke <felekebrook05@gmail.com>
2022-02-17 15:48:17 +0300

    m

--- src/js/landing.js
+++ src/js/landing.js
@@ -1,6 +1,6 @@
 const navbar = document.querySelector(".nav-bar");
 const sectionOne = document.querySelector(".header-container");
-
+const links = document.querySelector(".links")
 const sectionOneOptions = {
     roorMargin: "200px 0px 0px 0px"
 };
@@ -13,8 +13,11 @@ const sectionOneObserver = new IntersectionObserver(function(
     entries.forEach(entry => {
        if(!entry.isIntersecting){
            navbar.classList.add("nav-bar-on-white")
+           links.classList.add("links-on-white");
        } else {
             navbar.classList.remove("nav-bar-on-white");
+           links.classList.remove("links-on-white");
+
        }
     });
 },
