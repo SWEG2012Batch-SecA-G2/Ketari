a94cb6f3ce36699da006108bd3c8ad4ec16eaeb1
Dagmawi Babi <44819176+dagmawibabi@users.noreply.github.com>
2022-02-19 17:31:45 +0300

    added undo function to posting jobs

--- src/js/postJob.js
+++ src/js/postJob.js
@@ -1,12 +1,17 @@
 
 function postJob(){
+    let companyLogo = document.getElementById("companyLogo").value;
     let companyName = document.getElementById("companyName").value;
     let location = document.getElementById("location").value;
     let jobRole = document.getElementById("jobRole").value;
     let jobType = document.getElementById("jobType").value;
     let description = document.getElementById("description").value;
+    let submitJobBtn = document.getElementById("submitJobBtn");
+    let undoSubmissionBtn = document.getElementById("undoSubmissionBtn");
+    
 
     let newJobObj = {
+        "logo": companyLogo,
         "company": companyName,
         "location": location,
         "jobRole": jobRole,
@@ -14,14 +19,30 @@ function postJob(){
         "description": description,
     }
 
+    // Add New Job to local storage
     let oldJobsList = JSON.parse(localStorage.getItem("jobs"));
     oldJobsList.push(newJobObj);
     localStorage.clear();
     localStorage.setItem("jobs", JSON.stringify(oldJobsList));
-    
+
+    // Show notification and undo button
+    submitJobBtn.style.backgroundColor = "Green";
+    submitJobBtn.innerText = "Posted!";
+    submitJobBtn.onclick = () => {};
+    undoSubmissionBtn.style.display = "block";    
 }
 
-
+function undoPostJob(){
+    let oldJobsList = JSON.parse(localStorage.getItem("jobs"));
+    oldJobsList.pop();
+    localStorage.clear();
+    localStorage.setItem("jobs", JSON.stringify(oldJobsList));
+    // Show notification and undo button
+    submitJobBtn.style.backgroundColor = "#cf566a";
+    submitJobBtn.innerText = "Submit New Job!";
+    submitJobBtn.onclick = postJob;
+    undoSubmissionBtn.style.display = "none";    
+}
 
 
 
