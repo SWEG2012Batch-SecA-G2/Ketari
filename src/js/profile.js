c0b94d99d5c41733611f6deed36bf6efefc0f35c
Dawit-Getachew <69157562+Dawit-Getachew@users.noreply.github.com>
2022-02-20 12:04:58 +0300

    localstorage obj

--- src/js/profile.js
+++ src/js/profile.js
@@ -1,18 +1,4 @@
-        
-    var region = document.forms["myForm"]["region"].value;
-    var profession = document.forms["myForm"]["profession"].value;
-    var smedia = document.forms["myForm"]["smedia"].value;
-    var skill2 = document.forms["myForm"]["skill2"].value;
-    var skill3 = document.forms["myForm"]["skill3"].value;
-    var sRefName = document.forms["myForm"]["sRefName"].value;
-    var sRefPhone = document.forms["myForm"]["sRefPhone"].value;
-    var tRefName = document.forms["myForm"]["tRefName"].value;
-    var tRefPhone = document.forms["myForm"]["tRefPhone"].value;
 
-    var slanguage = document.forms["myForm"]["slanguage"].value;
-    var sLangLevel = document.forms["myForm"]["slevel"].value;
-    var skillLevel2 = document.forms["myForm"]["skillLevel2"].value;
-    var skillLevel3 = document.forms["myForm"]["skillLevel3"].value;
 // Profile Page form validation
 function validate() {
     var fname = document.forms["myForm"]["fname"].value;
@@ -129,4 +115,89 @@ function validate() {
         alert("Please enter your first reference phone number");
         return false;
     }
+}
+function store(){
+    var region = document.forms["myForm"]["region"].value;
+    var profession = document.forms["myForm"]["profession"].value;
+    var smedia = document.forms["myForm"]["smedia"].value;
+    var skill2 = document.forms["myForm"]["skill2"].value;
+    var skill3 = document.forms["myForm"]["skill3"].value;
+    var sRefName = document.forms["myForm"]["sRefName"].value;
+    var sRefPhone = document.forms["myForm"]["sRefPhone"].value;
+    var tRefName = document.forms["myForm"]["tRefName"].value;
+    var tRefPhone = document.forms["myForm"]["tRefPhone"].value;
+    var slanguage = document.forms["myForm"]["slanguage"].value;
+    var sLangLevel = document.forms["myForm"]["slevel"].value;
+    var skillLevel2 = document.forms["myForm"]["skillLevel2"].value;
+    var skillLevel3 = document.forms["myForm"]["skillLevel3"].value;
+    var fname = document.forms["myForm"]["fname"].value;
+    var lname = document.forms["myForm"]["lname"].value;
+    var phoneNumber = document.forms["myForm"]["pnumber"].value;
+    var age = document.forms["myForm"]["age"].value;
+    var country = document.forms["myForm"]["country"].value;
+    var city = document.forms["myForm"]["city"].value;
+    var zcode = document.forms["myForm"]["zcode"].value;
+    var jtitle = document.forms["myForm"]["jtitle"].value;
+    var employer = document.forms["myForm"]["employer"].value;
+    var jobStartDate = document.forms["myForm"]["jobStartDate"].value;
+    var jobEndDate = document.forms["myForm"]["jobEndDate"].value;
+    var sname = document.forms["myForm"]["sname"].value;
+    var slocation = document.forms["myForm"]["slocation"].value;
+    var schoolStartDate = document.forms["myForm"]["schoolStartDate"].value;
+    var schoolEndDate = document.forms["myForm"]["schoolEndDate"].value;
+    var degree = document.forms["myForm"]["degree"].value;
+    var skill1 = document.forms["myForm"]["skill1"].value;
+    var skillLevel1 = document.forms["myForm"]["skillLevel1"].value;
+    var flanguage = document.forms["myForm"]["flanguage"].value;
+    var fLangLevel = document.forms["myForm"]["fLangLevel"].value;
+    var fRefName = document.forms["myForm"]["fRefName"].value;
+    var fRefPhone = document.forms["myForm"]["fRefPhone"].value;
+
+    let profileObj = {
+        "fname": fname,
+        "lname": lname,
+        "phoneNumber": phoneNumber,
+        "age": age,
+        "country": country,
+        "city": city,
+        "zcode": zcode,
+        "jtitle": jtitle,
+        "employer": employer,
+        "jobStartDate": jobStartDate,
+        "jobEndDate": jobEndDate,
+        "sname": sname,
+        "slocation": slocation,
+        "schoolStartDate": schoolStartDate,
+        "schoolEndDate": schoolEndDate,
+        "degree": degree,
+        "skill1": skill1,
+        "skillLevel1": skillLevel1,
+        "flanguage": flanguage,
+        "fLangLevel": fLangLevel,
+        "fRefName": fRefName,
+        "fRefPhone": fRefPhone,
+        "region": region,
+        "profession": profession,
+        "smedia": smedia,
+        "skill2": skill2,
+        "skill3": skill3,
+        "sRefName": sRefName,
+        "sRefPhone": sRefPhone,
+        "tRefName": tRefName,
+        "tRefPhone": tRefPhone,
+        "slanguage": slanguage,
+        "sLangLevel": sLangLevel,
+        "skillLevel2": skillLevel2,
+        "skillLevel3": skillLevel3
+    }
+
+    localStorage.setItem("profile", JSON.stringify(profileObj));
+    let oldProfile = JSON.parse(localStorage.getItem("profile"));
+    oldProfile.push(profileObj);
+    localStorage.clear();
+    localStorage.setItem("profile", JSON.stringify(oldProfile));
+}
+
+function loadData(){
+    
 }
